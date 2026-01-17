52652af90217d06e6a9371946b677256a6fed919
rifathasan404-blip <gg281692@gmail.com>
2026-01-17 10:19:20 +0600

    DATETIME-1.sql "-- This query retrieves the creation time of orders in a clear and formatted manner. "Day Sunday January Q1 2025 23:22:04 PM"

---
+++ DATETIME-1.sql
@@ -0,0 +1,12 @@
+-- This query retrieves the creation time of orders in a clear and formatted manner.
+--"Day Sunday January Q1 2025 23:22:04 PM"           
+
+SELECT
+
+'Day ' + 
+DATENAME(WEEKDAY, CreationTime) + ' ' +
+DATENAME(MONTH, CreationTime) + ' Q' +
+CAST(DATEPART(QUARTER, CreationTime) AS VARCHAR(1)) + 
+FORMAT(CreationTime, ' yyyy HH:mm:ss tt') AS [A clear format]
+
+FROM Sales.Orders
